89da494d237968465a3d3dd7d132cc76e746f083
Йэориос <egor.zyrianov@gmail.com>
2021-07-24 00:04:38 +0700

    some readable comments
    24072021 004

--- client.py
+++ client.py
@@ -1,49 +1,47 @@
 import asyncio
 import os
-
-async def tcp_echo_client(director = "source_data"):
+#echo machine
+async def tcp_echo_client(director = "source_data", package_size = 1000):
     reader, writer = await asyncio.open_connection(
         '127.0.0.1', 8888)
-
+#make file iterable
     def bite(inp):
         while True:
-            data = inp.read(1000)
+            data = inp.read(package_size)
             if not data:
                 break
             yield data
-
+#case of direct file accessing
     if os.path.isfile(director):
         with open(director, 'r') as fd:
             for clod in bite(fd):
                 writer.write(clod.encode())
                 await writer.drain()
-
-
+#case of search with file name
     else:
         tree = os.walk(os.getcwd())
         files = []
-
-        for dirs in tree: #any file with given name will be found
+    #explore directory space
+        for dirs in tree:   #any file with given name will be found
             for files_name in dirs[2]:
                 if director in files_name:
                     files.append(dirs[0]+'/'+files_name)
-
+    #repeatedly send a package_size data until it's come to an end
         for file in files:  #all of them will be send part by part
             with open(file, 'r') as fd:
                 for clod in bite(fd):
                     writer.write(clod.encode())
                     await writer.drain()
-        writer.write("end".encode())
+        writer.write("end".encode())    #end code
         await writer.drain()
 
-###########responce part##############
+#responce part
     response = await reader.read(100)
     print(f'Received: {response.decode()!r}')
-
-    print('Close the connection')
     writer.close()
     await writer.wait_closed()
+    print('Connection closed')
 
 #---------------------------------------------#
-
+#running that machine
 asyncio.run(tcp_echo_client())
