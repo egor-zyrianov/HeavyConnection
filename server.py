e66c7b292de4c1f43767cef9131f8b4092e6b068
Йэориос <egor.zyrianov@gmail.com>
2021-07-24 00:17:24 +0700

    Some readable comments
    24072021 014

--- server.py
+++ server.py
@@ -1,16 +1,19 @@
 import asyncio
+#HOST = ('127.0.0.1', 8888)
+FILE_FLAG = 'w+' #write and read (could be 'a' - add)
 
-async def handle_echo(reader, writer):
-   data = await reader.read(10000)
+#data receiver
+async def handle_echo(reader, writer, package_size = 10000):
+ #initial package (thinking about rework)
+   data = await reader.read(package_size)
    message = data.decode()
    addr = writer.get_extra_info('peername')
-
+ #with creating file, data receiving till the end 
    directory = "data/source_"+addr[0]+"_data.txt"
-
-   with open(directory, 'w+') as f:
+   with open(directory, FILE_FLAG) as f:
       f.write(message)
       while True:
-         data = await reader.read(10000)
+         data = await reader.read(package_size)
          print(data)
          if data[-3::] == "end".encode():
             message = data.decode()
@@ -19,15 +22,16 @@ async def handle_echo(reader, writer):
          message = data.decode()
          f.write(message)
       print(f"Received successful from {addr!r}")
-
+ #sending code of the result
    response = {"code": 200}
    print(f"response: {response!r}")
    writer.write(str(response).encode())
    await writer.drain()
 
-   print("Close the connection")
    writer.close()
+   print("Connection closed")
 
+#running the server
 async def main():
    server = await asyncio.start_server(
        handle_echo, '127.0.0.1', 8888)
@@ -37,6 +41,7 @@ async def main():
 
    async with server:
        await server.serve_forever()
-
+         
+#running the machine
 if __name__ == '__main__':
    asyncio.run(main())
