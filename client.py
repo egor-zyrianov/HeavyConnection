3766f775ada4c8585e01be07898970da43cc9ed4
Йэориос <egor.zyrianov@gmail.com>
2021-08-01 09:11:26 +0700

    01082021
    update: file manager, connection protocol.
    improved stability overall

--- client.py
+++ client.py
@@ -1,47 +1,97 @@
 import asyncio
-import os
-#echo machine
-async def tcp_echo_client(director = "source_data", package_size = 1000):
+from os import walk
+from os import path
+import errno
+
+BUFFER_SIZE = 500
+DIRECTORY = "source_data/"
+FILE_DATA = "source_data"
+
+
+
+#iterate through the file
+#parameters: (pathlike obj, read length (bits))
+#output: (str)
+def bite(inp):
+    while True:
+        data = inp.read(BUFFER_SIZE)
+        if not data:
+            break
+        yield data
+
+
+
+#search by key word
+#parameters: (str, str\pathlike obj)
+#output: (nested array)
+def find(keyWord, director = DIRECTORY):
+   result = []
+   for root, dirs, files in walk(director):
+       for name in files:
+          if keyWord in name:
+             result.append(path.join(root, name))
+   return result
+
+
+
+###############################################
+#parameters: (asyncio connection pair, str)
+#output: (int\Error)
+def send_data(connection, file_name=FILE_DATA):
+    writer = connection[1]
+    #-----------------------------------------
+    async def hand_over(directory, wr = writer):
+        with open(directory, 'r') as fd:
+            print("data directory", fd.name)
+            count = 0
+            for clod in bite(fd):
+                count += 1
+                print("clod", count)
+                wr.write(clod.encode())
+                await wr.drain()
+        return 200
+    # -----------------------------------------
+    if path.isfile(file_name):
+        result = hand_over(file_name)
+        return result
+    # -----------------------------------------
+    if path.isfile(DIRECTORY + file_name):
+        result = hand_over(DIRECTORY +file_name)
+        return result
+    # -----------------------------------------
+    files = find(file_name, DIRECTORY)
+    result = None
+    for file in files:
+        print("data on stage of upload\n", file)
+        if path.isfile(file):
+            result = hand_over(file)
+        else:
+            print("file upload failed: \n", file)
+    return result
+
+
+
+#client
+#parameters: non
+#output: non
+async def tcp_echo_client():
     reader, writer = await asyncio.open_connection(
         '127.0.0.1', 8888)
-#make file iterable
-    def bite(inp):
-        while True:
-            data = inp.read(package_size)
-            if not data:
-                break
-            yield data
-#case of direct file accessing
-    if os.path.isfile(director):
-        with open(director, 'r') as fd:
-            for clod in bite(fd):
-                writer.write(clod.encode())
-                await writer.drain()
-#case of search with file name
-    else:
-        tree = os.walk(os.getcwd())
-        files = []
-    #explore directory space
-        for dirs in tree:   #any file with given name will be found
-            for files_name in dirs[2]:
-                if director in files_name:
-                    files.append(dirs[0]+'/'+files_name)
-    #repeatedly send a package_size data until it's come to an end
-        for file in files:  #all of them will be send part by part
-            with open(file, 'r') as fd:
-                for clod in bite(fd):
-                    writer.write(clod.encode())
-                    await writer.drain()
-        writer.write("end".encode())    #end code
-        await writer.drain()
-
-#responce part
-    response = await reader.read(100)
+    # ---------------------------------------------
+    writer.write("POST".encode())
+    await writer.drain()
+    response = await reader.read(BUFFER_SIZE)
     print(f'Received: {response.decode()!r}')
+    if "200".encode() in response:
+        result = await send_data((reader, writer))
+        writer.write("END".encode())
+    # ---------------------------------------------
+    response = await reader.read(BUFFER_SIZE)
+    print(f'Received: {response.decode()!r}')
+    print('Close the connection')
     writer.close()
     await writer.wait_closed()
-    print('Connection closed')
-
-#---------------------------------------------#
-#running that machine
-asyncio.run(tcp_echo_client())
+###############################################
+#start the program
+if __name__ == '__main__':
+   asyncio.run(tcp_echo_client())
--- server.py
+++ server.py
@@ -1,47 +1,182 @@
 import asyncio
-#HOST = ('127.0.0.1', 8888)
-FILE_FLAG = 'w+' #write and read (could be 'a' - add)
+from os import walk
+from os import path
+from os import makedirs
+from datetime import datetime
 
-#data receiver
-async def handle_echo(reader, writer, package_size = 10000):
- #initial package (thinking about rework)
-   data = await reader.read(package_size)
-   message = data.decode()
-   addr = writer.get_extra_info('peername')
- #with creating file, data receiving till the end 
-   directory = "data/source_"+addr[0]+"_data.txt"
-   with open(directory, FILE_FLAG) as f:
-      f.write(message)
-      while True:
-         data = await reader.read(package_size)
-         print(data)
-         if data[-3::] == "end".encode():
+DIRECTORY = "data/"
+STORAGE = "storage/"
+RESPONSE_FILE = None #DIRECTORY + "youtubePage.html"
+FILE_ACCESS_SPEC = None
+BUFFER_SIZE = 10000
+
+
+
+#search by key word
+#parameters: (str, str\pathlike obj)
+#output: (nested array)
+def find(keyWord, director = DIRECTORY):
+   result = []
+   for root, dirs, files in walk(director):
+       for name in files:
+          if keyWord in name:
+             result.append(path.join(root, name))
+   return result
+
+
+
+#iterate through the file
+#parameters: (pathlike obj, read length (bits))
+#output: (str)
+def bite(inp, bite_size = BUFFER_SIZE):
+   while True:
+      data = inp.read(bite_size)
+      if not data:
+         break
+      yield data
+
+
+
+#save loaded data from connected client
+#parameters: (asyncio connecton pair, str, str)
+#output: (int\Error)
+async def load_data(connection,
+              save_dir = STORAGE,
+              client = "anon"):
+   reader = connection[0]
+   errors = []
+   #------------------------------------------------------------
+   directory = save_dir
+   if not path.isfile(save_dir):
+      directory += client +datetime.now().strftime("%d%m%Y_%H%M_data.txt")
+      makedirs(path.dirname(directory), exist_ok=True)
+   #------------------------------------------------------------
+   try:
+      with open(directory, 'w+') as fd:
+         print("storage directory", fd.name)
+         count = 0
+         while True:
+            count += 1
+            print("received data package", count)
+            data = await reader.read(BUFFER_SIZE)
+            if (data[-3::] == "END".encode()
+            or data is None
+            or data == ""
+            or not data):
+               try:
+                  message = data.decode()
+                  fd.write(message)
+               except Exception as error_0:
+                  errors.append(error_0)
+               break
             message = data.decode()
-            f.write(message)
-            break
-         message = data.decode()
-         f.write(message)
-      print(f"Received successful from {addr!r}")
- #sending code of the result
-   response = {"code": 200}
-   print(f"response: {response!r}")
-   writer.write(str(response).encode())
-   await writer.drain()
+            fd.write(message)
+         print(f"Received successful from {client!r}")
+   except Exception as error:
+      return error
+   #------------------------------------------------------------
+   return 200
 
-   writer.close()
-   print("Connection closed")
 
-#running the server
+
+#parameters: (asyncio connection pair, str)
+#output: (int\Error)
+def send_data(connection, file_name=RESPONSE_FILE):
+   writer = connection[1]
+   # -----------------------------------------
+   async def hand_over(directory, wr=writer):
+      with open(directory, 'r') as fd:
+         print("data directory", fd.name)
+         count = 0
+         for clod in bite(fd):
+            count += 1
+            print("clod", count)
+            wr.write(clod.encode())
+            await wr.drain()
+      return 200
+   # -----------------------------------------
+   if path.isfile(file_name):
+      result = hand_over(file_name)
+      return result
+   # -----------------------------------------
+   if path.isfile(DIRECTORY + file_name):
+      result = hand_over(DIRECTORY + file_name)
+      return result
+   # -----------------------------------------
+   files = find(file_name, DIRECTORY)
+   result = None
+   if files.__len__() > 1:
+      print("There is a several files:\n",
+            files,"\n,I give you first one")
+      #print("yes/no?\n")
+      #if input() == "no":
+      #  return 200
+   print("data on stage of upload\n", files[0])
+   if path.isfile(files[0]):
+      result = hand_over(files[0])
+   else:
+      print("file upload failed: \n", files[0])
+   return result
+
+
+
+#ways to request file
+#parameters: (str)
+#output: (str)
+def file_declaration(flag):
+   if not flag:
+      if not RESPONSE_FILE:
+         return 300
+      else:
+         return DIRECTORY + RESPONSE_FILE
+   else:
+      if ("__keyboard__" in flag
+              or "k" in flag):
+         return input()
+      return flag
+
+
+
+###########################################
+#server
+#parameters: (asyncio connection pair)
+#output: non
+async def handle_echo(reader, writer):
+   data = await reader.read(BUFFER_SIZE)
+   addr = writer.get_extra_info('peername')
+   # -----------------------------------------
+   message = data.decode()
+   if "GET" in message:
+      result = send_data(
+         (reader, writer), file_declaration(FILE_ACCESS_SPEC))
+   if "POST" in message:
+      response = 200  # !
+      print(f"end code: {response!r}")  # !
+      writer.write(str(response).encode())  # !
+      await writer.drain()  # !
+      result = await load_data(
+         (reader, writer))
+   # -----------------------------------------
+   response = 200#!
+   print(f"end code: {response!r}")#!
+   writer.write(str(response).encode())#!
+   await writer.drain()#!
+   print("Close the connection")#!
+   writer.close()#!
+
+
+
+#main program
 async def main():
    server = await asyncio.start_server(
        handle_echo, '127.0.0.1', 8888)
-
    addr = server.sockets[0].getsockname()
    print(f'Serving on {addr}')
 
    async with server:
        await server.serve_forever()
-         
-#running the machine
+##############################################
+#start the program
 if __name__ == '__main__':
    asyncio.run(main())
+
